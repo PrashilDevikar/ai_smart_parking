d8d50aaa333d6ffcc39c50a0461a2d61717d5db1
PrashilDevikar <prashildevikar@gmail.com>
2026-09-03 18:18:55 +0530

    Remove redundant md:pl-64 padding to eliminate blank space between sidebar and page content

--- src/components/layout/OperatorShell.tsx
+++ src/components/layout/OperatorShell.tsx
@@ -26,7 +26,7 @@ export function OperatorShell({ children }: { children: React.ReactNode }) {
       <OperatorSidebar />
       <div className="flex-1 flex flex-col min-w-0">
         <Header user={user} />
-        <main className="flex-1 p-4 md:p-6 lg:p-8 w-full max-w-[1600px]">
+        <main className="flex-1 p-4 md:p-8 max-w-7xl w-full mx-auto">
           {children}
         </main>
       </div>
